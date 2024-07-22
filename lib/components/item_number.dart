1063baa64f3028fd8110c2e88da09c31aee2a58a
yasser-osamaa <yasserosamaa88@gmail.com>
2024-07-22 20:35:24 +0300

    change Icon to Icon Button to click it

--- lib/components/item_number.dart
+++ lib/components/item_number.dart
@@ -8,49 +8,53 @@ class ItemNumber extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 100,
-            color: const Color(0xffFFA041),
-            child: Row(
+      height: 100,
+      color: const Color(0xffFFA041),
+      child: Row(
+        children: [
+          Container(
+            color: const Color(0xffFFFDE5),
+            child: Image.asset(object.image),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  color: const Color(0xffFFFDE5),
-                  child: Image.asset(object.image),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 18),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        object.jpText,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                        ),
-                      ),
-                      Text(
-                        object.enText,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                        ),
-                      ),
-                    ],
+                Text(
+                  object.jpText,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
                   ),
                 ),
-                const Spacer(
-                  flex: 1,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(right: 18),
-                  child: Icon(
-                    Icons.play_arrow,
+                Text(
+                  object.enText,
+                  style: const TextStyle(
                     color: Colors.white,
-                    size: 30,
+                    fontSize: 22,
                   ),
                 ),
               ],
             ),
-          ) ;
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 18),
+            child: IconButton(
+              highlightColor: const Color(0xff503B33),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
